2cfd109ea1c69d75b28d35112a269703cdc55588
Max Aldunate <maxaldunate@gmail.com>
2017-12-22 12:42:45 +0100

    organizing templates: dzarq, netowrking & servers

---
+++ dzarq/templates/run.servers.bat
@@ -0,0 +1,8 @@
+@echo off
+
+aws cloudformation create-stack^
+  --stack-name "dzarq-improvements-servers"^
+  --template-body file://servers.json^
+  --parameters ParameterKey="ProjectName",ParameterValue="dzarq"^
+               ParameterKey="Prefix",ParameterValue="dz"^
+			   ParameterKey="VpcId",ParameterValue=""
