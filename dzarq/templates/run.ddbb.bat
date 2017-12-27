68883559a9c75ee042a8a8b7b0215e258cccf332
Max Aldunate <maxaldunate@gmail.com>
2017-12-27 19:30:14 +0100

    network and ddbb ready

--- dzarq/templates/run.ddbb.bat
+++ dzarq/templates/run.ddbb.bat
@@ -5,6 +5,9 @@ aws cloudformation create-stack^
   --template-body file://ddbb.json^
   --parameters ParameterKey="ProjectName",ParameterValue="dzarq"^
                ParameterKey="Prefix",ParameterValue="dz"^
+			   ParameterKey="VpcId",ParameterValue="vpc-acd233ca"^
+			   ParameterKey="RdsAccessSecurityGroup",ParameterValue="sg-e188a59a"^
+			   ParameterKey="SubnetsIds",ParameterValue="subnet-18612943\,subnet-e9bfb38e\,subnet-2bc7d762"^
 			   ParameterKey="AZone",ParameterValue="eu-west-1a"^
 			   ParameterKey="DBName",ParameterValue="db363458885"^
 			   ParameterKey="DBUser",ParameterValue="arnoldo"^
