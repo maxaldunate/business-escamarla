e6055d0742244ceb900a4802ac4bc1f53855667f
Max Aldunate <maxaldunate@gmail.com>
2017-12-28 17:05:32 +0100

    everything ready... even web servers

--- dzarq/templates/run.ddbb.bat
+++ dzarq/templates/run.ddbb.bat
@@ -5,9 +5,9 @@ aws cloudformation create-stack^
   --template-body file://ddbb.json^
   --parameters ParameterKey="ProjectName",ParameterValue="dzarq"^
                ParameterKey="Prefix",ParameterValue="dz"^
-			   ParameterKey="VpcId",ParameterValue="vpc-acd233ca"^
-			   ParameterKey="RdsAccessSecurityGroup",ParameterValue="sg-e188a59a"^
-			   ParameterKey="SubnetsIds",ParameterValue="subnet-18612943\,subnet-e9bfb38e\,subnet-2bc7d762"^
+			   ParameterKey="VpcId",ParameterValue="vpc-e34baa85"^
+			   ParameterKey="RdsAccessSecurityGroup",ParameterValue="sg-c793bfbc"^
+			   ParameterKey="SubnetsIds",ParameterValue="subnet-86c68edd\,subnet-dc101fbb\,subnet-55a1b11c"^
 			   ParameterKey="AZone",ParameterValue="eu-west-1a"^
 			   ParameterKey="DBName",ParameterValue="db363458885"^
 			   ParameterKey="DBUser",ParameterValue="arnoldo"^
